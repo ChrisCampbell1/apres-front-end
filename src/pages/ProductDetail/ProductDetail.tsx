f0ce09344afa411ae1fdf6f609bc1a8ddf1550fa
Chris Campbell <christopherecampbell@gmail.com>
2023-02-27 14:59:08 -0700

    add type narrowing on product detail page

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -56,53 +56,59 @@ const ProductDetail = (): JSX.Element => {
 
   return (  
     <div className={styles.container}>
-      <div className={styles.sideBySide}>
-        <div>
-          <img src={listing?.image} alt={`${listing?.category}`} />
-        </div>
-        <div>
-          <h1>{listing?.title}</h1>
-          <p>{listing?.status}</p>
-          <p><span>Listed by: </span><Link to={`/profiles/${listing?.sellerId}`} id={styles.sellerLink}>{listing?.seller.name}</Link> on {listing?.createdAt.slice(0,10)}</p>
-          <p><span>Description:</span></p>
-          <p>{listing?.description}</p>
-          <p><span>Price: </span>${listing?.price}</p>
-          <p><span>Condition: </span>{arr.map((el: number): JSX.Element => (
-          el <= listing?.condition ?
-          <img
-            id={styles.snowflake}
-            key={el}
-            src={snowflake} 
-            alt="Snowflake Symbol"
-          />
-          :
-          <></>
-          ))}
-        </p>
-          <p><span>Manufacturer: </span>{listing?.manufacturer}</p>
-          <p><span>Year Manufactured: </span>{listing?.yearManufactured}</p>
-          <p><span>Dimensions: </span>{listing?.dimensions}</p>
-          <p><span>Material: </span>{listing?.material}</p>
-          {(profileId !== undefined && listing?.status === "For Sale" && profileId !== listing?.sellerId) 
-            ?
-            <button onClick={() => handlePurchaseClick()} id={styles.purchase}>Purchase</button>
-            :
-            (listing?.status === "For Sale" && profileId !== listing?.sellerId) ?
-              <p>Please log in to make a purchase</p>
-              :
-              <></>
-          }
-          {(profileId === listing?.sellerId)
-          ?
-          <div className={styles.buttons}>
-            <Link to={`/listings/${listing?.id}/edit`} state={listing}>Edit Listing</Link>
-            <button onClick={() => handleDelete()} id={styles.delete}>Delete Listing</button>        
+      {listing ?
+        <div className={styles.sideBySide}>
+          <div>
+            <img src={listing?.image} alt={`${listing?.category}`} />
           </div>
-          :
-          <></>
-          }
+          <div>
+            <h1>{listing?.title}</h1>
+            <p>{listing?.status}</p>
+            <p><span>Listed by: </span><Link to={`/profiles/${listing?.sellerId}`} id={styles.sellerLink}>{listing?.seller.name}</Link> on {listing?.createdAt.slice(0,10)}</p>
+            <p><span>Description:</span></p>
+            <p>{listing?.description}</p>
+            <p><span>Price: </span>${listing?.price}</p>
+            <p><span>Condition: </span>{arr.map((el: number): JSX.Element => (
+            el <= listing?.condition ?
+            <img
+              id={styles.snowflake}
+              key={el}
+              src={snowflake} 
+              alt="Snowflake Symbol"
+            />
+            :
+            <></>
+            ))}
+          </p>
+            <p><span>Manufacturer: </span>{listing?.manufacturer}</p>
+            <p><span>Year Manufactured: </span>{listing?.yearManufactured}</p>
+            <p><span>Dimensions: </span>{listing?.dimensions}</p>
+            <p><span>Material: </span>{listing?.material}</p>
+            {(profileId !== undefined && listing?.status === "For Sale" && profileId !== listing?.sellerId) 
+              ?
+              <button onClick={() => handlePurchaseClick()} id={styles.purchase}>Purchase</button>
+              :
+              (listing?.status === "For Sale" && profileId !== listing?.sellerId) ?
+                <p>Please log in to make a purchase</p>
+                :
+                <></>
+            }
+            {(profileId === listing?.sellerId)
+            ?
+            <div className={styles.buttons}>
+              <Link to={`/listings/${listing?.id}/edit`} state={listing}>Edit Listing</Link>
+              <button onClick={() => handleDelete()} id={styles.delete}>Delete Listing</button>        
+            </div>
+            :
+            <></>
+            }
+          </div>
         </div>
-      </div>
+        :
+        <div>
+          <h1>Loading...</h1>
+        </div>  
+    }
     </div>
   )
 }
