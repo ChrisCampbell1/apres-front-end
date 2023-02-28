5c070c913529b8adfed759f024bc87919b90964b
Chris Campbell <christopherecampbell@gmail.com>
2023-02-28 15:56:12 -0700

    style landing page

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 // assets and stylesheets
 import styles from './Landing.module.css'
-import heroVideo from '/assets/hero-video.mp4'
 
 // types
 import { User } from '../../types/models'
@@ -17,29 +16,32 @@ const Landing = (props: LandingProps): JSX.Element => {
 
   return (
     <main className={styles.container}>
-      {/* <img src={hero} alt="apres-logo" id={styles.hero}/> */}
-      <video src={heroVideo}
-        playsInline
-        autoPlay
-        muted
-        loop
-        id={styles.hero}>
-      </video>
-      <h1>Welcome to the premiere marketplace for buying and selling used ski and snowboard gear.</h1>
-      <div className={styles.about}>
-        <article>
-          Looking to buy used ski or snowboard equipment? You're at the right place. Browse hundreds of local listings to find the best deals on the gear you need.
-          <div className='buttons'>
-            <Link to={'/all'}>Shop Now</Link>
+      <div className={styles.landingHero}>
+        <div className={styles.text}>
+          <div className={styles.colorblock}>
+            <div className={styles.cta}>
+              <h1>Browse listings now</h1>
+              <div className={styles.buttons}>
+                <Link to={'/all'}>Shop Now</Link>
+              </div>
+            </div>
+            <div className={styles.cta}>
+              <h1>List your gear</h1>
+              <div className={styles.buttons}>
+                {user ?
+                <>
+                <Link to={'/listings/new'}>Create Listing</Link>
+                </>
+                :
+                <>
+                <Link to={'/signup'}>Sign Up</Link>
+                <Link to={'/login'}>Log In</Link>
+                </>
+                }
+              </div>
+            </div>
           </div>
-        </article>
-      <article>
-        Not sure what to do with those skis that have been sitting in your quiver unsed for the past few seasons? Create an account on après to list your gear and connect with other local skiers and snowboarders who are shoping for used gear.
-        <div className='buttons'>
-          <Link to={'/signup'}>Sign Up</Link>
-          <Link to={'/login'}>Log In</Link>
         </div>
-      </article>
       </div>
     </main>
   )
